73e4c1566d0fa059f9606035cb242ca445a4da8a
Soumil07 <soumil.paranjpay@gmail.com>
2017-09-19 18:29:05 +0530

    Fix event

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -2,8 +2,8 @@ exports.run = (client, guild) => {
   if (!guild.available) return;
   client.settings.guilds.create(guild).catch(e => client.emit("log", e, "error"));
     client.channels.get("336452692178108446").send(`Joined ${guild.name}! Currently in ${client.guilds.size} servers!`)
-  const channel = guild.channels.filter(c=> c.permissionsFor(guild.defaultRole).has("SEND_MESSAGES")).sort((a,b) => a.position - b.postion).first();
-  channel.send(`= Apex Botchelon =
+  const channel = guild.channels.filter(c=> c.permissionsFor(guild.me).has("SEND_MESSAGES")).sort((a,b) => a.position - b.postion).first();
+  channel.send(`= Apex Bot =
     • Thanks for inviting me to your guild! Let's hope I'm really useful to you!
     • I'm assuming you've invited me with the correct permissions. If a command doesn't work for any apparent reason, check my permissions again.
     • To use moderator commands: Configure your moderator roles using -conf set modRole <yourModRoleName>. Use -conf list to check other mod, admin roles, and prefixes.
