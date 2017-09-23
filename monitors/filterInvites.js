f9aba22b422b57ff2dbce248c955286adbf0047b
Soumil07 <soumil.paranjpay@gmail.com>
2017-09-23 16:59:21 +0530

    codacy

--- monitors/filterInvites.js
+++ monitors/filterInvites.js
@@ -9,6 +9,6 @@ exports.run = (client, msg) => {
    if (msg.channel.type !== 'text' || msg.guild.settings.filterInvites !== true) return null;
         if (!/(https?:\/\/)?(www\.)?(discord\.(gg|li|me|io)|discordapp\.com\/invite)\/.+/.test(msg.content)) return null;
        msg.reply("Posting invite links isn't allowed on this server..")
-       msg.delete()
+       return msg.delete()
             .catch(err => client.emit('log', err, 'error'));
 };//
